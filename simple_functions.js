982939f81749ac7c2d2fd79ecfd25ad619e5de62
midesly <ayofrancesakin@gmail.com>
2022-10-05 09:47:32 -0400

    excercise1 complete

--- simple_functions.js
+++ simple_functions.js
@@ -1,1 +1,18 @@
-function display  
+function favoriteThings(){
+    console.log("music");
+    console.log("food");
+    console.log("traveling");
+}  
+
+function whyImhere(){
+    console.log("I need the job security");
+}
+
+function favoritePlace(){
+    console.log("I love the cty of Houston,mostly cause of how pretty it is");
+}
+
+// This is a call to the function
+favoriteThings()
+whyImhere()
+favoritePlace()
